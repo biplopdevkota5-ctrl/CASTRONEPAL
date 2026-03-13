4e11cdef2b9e3af62451b7a1a6bef58abfeaef2a
Hyper Nothing <officialhyper993@gmail.com>
2026-03-13 07:26:15 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,16 +1,8 @@
-
-'use client';
-
-/**
- * @fileOverview Firebase configuration object.
- * These values connect the client-side SDK to the Castro Nepal Firebase project.
- */
-
 export const firebaseConfig = {
-  apiKey: "AIzaSyDJ0wsKssy6xv091L8AAGLZyYzyu1jkp7U",
-  authDomain: "castro-hub-nepal.firebaseapp.com",
-  projectId: "castro-hub-nepal",
-  storageBucket: "castro-hub-nepal.firebasestorage.app",
-  messagingSenderId: "398444757666",
-  appId: "1:398444757666:web:b43ba9c0a04416733357fb"
+  "projectId": "castro-hub-nepal",
+  "appId": "1:39844757666:web:b43ba9c0a04416733357fb",
+  "apiKey": "AIzaSyDJOwsKssy6xvO91L8AAGLZyYzyu1jkp7U",
+  "authDomain": "castro-hub-nepal.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "39844757666"
 };
